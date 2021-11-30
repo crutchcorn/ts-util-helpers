3656484d7e5e0e828f78a951dda628dd7272e1cc
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-29 19:15:17 -0800

    fix: made object map typings more consistent

--- src/objects/object-map.ts
+++ src/objects/object-map.ts
@@ -1,15 +1,26 @@
+import {ObjectIterableCallback} from './shared'
+
 /**
  * Map through all keys in an object and call a CB. Assign that CB's return value to the key in question
  * @param object - The object to make this call on. This would otherwise be bound to `Object.prototype` but I don't like breaking the web
  * @param {objectMapCallback} cb - The callback in order to run the `Object.map` function
  * @returns - The object with it's keys altered by the cb param
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function objectMap<T extends Record<string, any>, R>(
+export function objectMap<
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  T extends Record<string, any>,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  CB extends ObjectIterableCallback<T, keyof T, any> = ObjectIterableCallback<
+    T,
+    keyof T,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    any
+  >,
+>(
   object: T,
-  cb: (value: T[keyof T], key: keyof T, object: T) => R,
+  cb: CB,
 ): {
-  [key in keyof T]: R extends infer Q ? Q : never
+  [key in keyof T]: ReturnType<CB> extends infer Q ? Q : never
 } {
   return Object.keys(object).reduce((prev, key) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
